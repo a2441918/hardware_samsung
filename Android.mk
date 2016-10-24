4914830c7aea7533b83136a065efd420b3c07b7d
shravankumarm <shaanjonesb@gmail.com>
2016-10-23 22:44:40 -0400

    1st commit

--- Android.mk
+++ Android.mk
@@ -6,28 +6,6 @@
 #
 #      http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-SAM_ROOT := $(call my-dir)
-
-# Exynos 4
-ifeq ($(TARGET_BOARD_PLATFORM),exynos4)
-ifeq ($(TARGET_SOC),exynos4210)
-include $(SAM_ROOT)/exynos4210.mk
-endif
-ifeq ($(TARGET_SOC),exynos4x12)
-include $(SAM_ROOT)/exynos4x12.mk
-endif
-endif
-
-# Exynos 3
-ifeq ($(TARGET_BOARD_PLATFORM),s5pc110)
-include $(SAM_ROOT)/s5pc110.mk
-endif
 
 # Wifi
 ifeq ($(BOARD_HAVE_SAMSUNG_WIFI),true)
