d6bb059883391c17dc00b5b3a41ca0fcc8ab7c71
shravankumarm <shaanjonesb@gmail.com>
2016-10-23 22:52:45 -0400

    mobile-filters

--- Android.mk
+++ Android.mk
@@ -15,3 +15,76 @@ endif
 ifeq ($(BOARD_VENDOR),samsung)
 include $(SAM_ROOT)/ril/Android.mk
 endif
+.modal-content {
+    background-color: #fefefe;
+    margin: auto;
+    padding: 5px;
+    border: 1px solid;
+    width: 40%;
+    text-align:center;
+	border-image: url("border.png") ;
+	border-width: 30px 30px 30px 30px;
+	border-image-slice: 25 22;
+}
+}
+.redirect-text{
+  color:#ff0000;
+  
+}
+/* The Close Button */
+.close {
+    color: #aaaaaa;
+    float: right;
+    font-size: 28px;
+    font-weight: normal;
+	margin-top:-30px;
+    
+}
+.close:hover,
+.close:focus {
+    color: #000;
+    text-decoration: none;
+    cursor: pointer;
+}
+</style>
+</head>
+<body>
+<!-- Trigger/Open The Modal -->
+<button id="myBtn">Open Modal</button>
+<!-- The Modal -->
+<div id="myModal" class="modal">
+  <!-- Modal content -->
+  <div class="modal-content">
+ 
+    <span class="close">x</span>
+    <img src="https://media.licdn.com/mpr/mpr/shrink_200_200/AAEAAQAAAAAAAAQQAAAAJGY3ZTQ3NzQ0LWRiM2QtNDEyNC04N2M0LTdhYzBiODYwOWM2Yg.png" height="50" />
+    <h2 class="redirect-text">REDIRECTING YOUR<br> SELECTION TO TICKETMASTER&reg;</h2>
+    <p>Pick and purchase your seats through ticketmaster and you'll be ready to go!</p>
+	<img src="http://www.moock.org/asdg/technotes/skinningV2ProgressBar/fig1.gif" />
+	<p>Copy Offer Code CHS to use in Ticketmaster? <input type="button" value="Copy to Clipboard" /></p>
+  </div>
+</div>
+<script>
+// Get the modal
+var modal = document.getElementById('myModal');
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+</script>
+</body>
+</html>
